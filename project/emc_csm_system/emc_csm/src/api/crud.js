0c572b372155d59592c4447e87f5522914604b40
VelvetShiki-V <--local>
2024-08-28 15:21:42 +0800

    csm reconstruct patch 2024.8.28

--- project/emc_csm_system/emc_csm/src/api/crud.js
+++ project/emc_csm_system/emc_csm/src/api/crud.js
@@ -2,55 +2,51 @@ import request from '@/utils/request'
 
 // 获取所有表名
 async function postFetchAll (postData) {
-  console.log('获取所有表 发送表单: ' + JSON.stringify(postData))
   return await request.post('/tables', postData)
 }
 
 // 获取单个表格结构
 async function postFetchSchema (postData) {
-  console.log('获取单个表结构 发送表单: ' + JSON.stringify(postData))
   return await request.post('/schema', postData)
 }
 
 // 获取单个表格数据
 async function postBrowse (postData) {
-  console.log('获取单个表数据 发送表单：' + JSON.stringify(postData))
   return await request.post('/browse', postData)
 }
 
 // 插入
 async function postCreate (postData) {
-  console.log('准备发送, payload: ' + JSON.stringify(postData))
   request.post('/set', postData)
 }
 
 // 更新
 async function postUpdate (postData) {
-  console.log('准备发送, payload: ' + JSON.stringify(postData))
   await request.post('/update', postData)
 }
 
 // 删除
 async function postDelete (postData) {
-  console.log('准备发送, payload: ' + JSON.stringify(postData))
   await request.post('/delete', postData)
+}
+
+// 登录获取token
+async function postLogin (postData) {
+  return await request.post('/emc/admin/login', postData)
 }
 
 // 登出
 async function postLogout (postData) {
-  console.log('logout发送表单' + JSON.stringify(postData))
   await request.post('/admin/user/logout', postData)
 }
 
 // token验证
 async function postVerify (postData) {
-  console.log('verify发送表单: ' + JSON.stringify(postData))
   return await request.post('/admin/user/verify', postData)
 }
 
 // 管理员验证
 async function postAdminVerift (postData) {
-  console.log('adminVerify发送表单: ' + JSON.stringify(postData))
   return await request.post('/admin/user/verifyadm', postData)
 }
 
@@ -61,6 +57,7 @@ export const emcRequest = {
   postUpdate,
   postDelete,
   postBrowse,
+  postLogin,
   postLogout,
   postVerify,
   postAdminVerift
--- project/emc_csm_system/emc_csm/src/store/modules/user.js
+++ project/emc_csm_system/emc_csm/src/store/modules/user.js
@@ -1,8 +1,7 @@
 import { storage } from '@/utils/storage'
 import { emcRequest } from '@/api/crud'
-import { Loading, Message } from 'element-ui'
-
-let eventSource
+// import { Loading, Message } from 'element-ui'
+// let eventSource
 
 export default {
   namespaced: true,
@@ -31,42 +30,46 @@ export default {
     },
     // SSE登录获取token
     async fetch_token_async (context, loginData) {
-      const load = Loading.service({
-        lock: true,
-        text: 'myEmc努力加载中',
-        background: 'rgba(0, 0, 0, 0.7)'
+      const ret = await emcRequest.postLogin(loginData)
+      context.commit('fetch_token', {
+        user: loginData.user,
+        token: ret.data
       })
-      // 创建事件流
-      const requestUrl = `https://cloud.myemc.net.cn/admin/user/login?user=${loginData.user}&password=${loginData.password}`
-      const sessionTokenPromise = new Promise((resolve, reject) => {
-        // 监听具名事件: session/token; 若监听的event为内置值, 则可以使用onmessage进行默认监听
-        eventSource = new EventSource(requestUrl)
-        eventSource.addEventListener('session/token', (e) => {
-          console.log('event: ', e)
-          console.log('received stream data: ', e.data)
-          context.commit('fetch_token', {
-            user: loginData.user,
-            token: e.data
-          })
-          load.close()
-          resolve('receive token success')
-        })
-        // 监听错误处理
-        eventSource.onerror = err => {
-          console.log('stream data received false: ', err)
-          Message.error('登录失败: SSE type error') // FIXME?
-          load.close()
-          eventSource.close()
-          reject(err)
-        }
-      })
-      // 持续监听
-      await sessionTokenPromise
+      // const load = Loading.service({
+      //   lock: true,
+      //   text: 'myEmc努力加载中',
+      //   background: 'rgba(0, 0, 0, 0.7)'
+      // })
+      // // 创建事件流
+      // const requestUrl = `https://cloud.myemc.net.cn/admin/user/login?user=${loginData.user}&password=${loginData.password}`
+      // const sessionTokenPromise = new Promise((resolve, reject) => {
+      //   // 监听具名事件: session/token; 若监听的event为内置值, 则可以使用onmessage进行默认监听
+      //   eventSource = new EventSource(requestUrl)
+      //   eventSource.addEventListener('session/token', (e) => {
+      //     console.log('event: ', e)
+      //     console.log('received stream data: ', e.data)
+      //     context.commit('fetch_token', {
+      //       user: loginData.user,
+      //       token: e.data
+      //     })
+      //     load.close()
+      //     resolve('receive token success')
+      //   })
+      //   // 监听错误处理
+      //   eventSource.onerror = err => {
+      //     console.log('stream data received false: ', err)
+      //     Message.error('登录失败: SSE type error') // FIXME?
+      //     load.close()
+      //     eventSource.close()
+      //     reject(err)
+      //   }
+      // })
+      // // 持续监听
+      // await sessionTokenPromise
     },
     // 登出: logout请求 + token清理 + 关闭监听事件
     async logout_async (context, logoutData) {
       await emcRequest.postLogout(logoutData)
-      if (eventSource) eventSource.close()
     },
     // token验证
     async token_verify_async (context, postData) {
